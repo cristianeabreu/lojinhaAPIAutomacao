dd2403304ffdbc3a3a6189cb133e8b8d919cb4bf
Cristiane Abreu <abreu.kr@gmail.com>
2022-05-15 22:58:33 +0100

    Initial commit

---
+++ src/test/java/dataFactory/ProdutoDataFactory.java
@@ -0,0 +1,40 @@
+package dataFactory;
+
+import pojo.ComponentePojo;
+import pojo.ProdutoPojo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProdutoDataFactory {
+    public static ProdutoPojo criarProdutoComumComOValorIgualA(double valor) {
+        ProdutoPojo produto = new ProdutoPojo();
+        produto.setProdutoNome("PlayStation 20");
+        produto.setProdutoValor(valor);
+
+        List<String> cores = new ArrayList<>();
+        cores.add("preto");
+        cores.add("branco");
+
+        produto.setProdutoCores(cores);
+        produto.setProdutoUrlMock("");
+
+        List<ComponentePojo> componentes = new ArrayList<>();
+
+        ComponentePojo componente = new ComponentePojo();
+        componente.setComponenteNome("Controle");
+        componente.setComponenteQuantidade(2);
+
+        ComponentePojo componente2 = new ComponentePojo();
+        componente2.setComponenteNome("Jogo de Aventura");
+        componente2.setComponenteQuantidade(1);
+
+        componentes.add(componente);
+        componentes.add(componente2);
+
+        produto.setComponente(componentes);
+
+        return produto;
+
+    }
+}
